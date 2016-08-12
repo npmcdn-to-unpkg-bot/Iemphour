987f4cc2d101040c49681d080803175986d88bea
Rawle Juglal <rawledjuglal@gmail.com>
2016-08-12 16:13:53 +0000

    new register route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Iemphour' });
 });
 
+router.get('/register', function(req, res, next){
+   res.render('register', {title:'Create New Account'}); 
+});
+
 module.exports = router;
